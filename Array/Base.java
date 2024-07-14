867aaf2072f44c135319fd9e448611a46fcb3b01
Jatin Sharma <mr.sharma.jatin@gmail.com>
2024-07-14 16:45:44 +0530

    first commit

---
+++ Array/Base.java
@@ -0,0 +1,12 @@
+
+/**
+ * Base
+ */
+public class Base {
+    // class can not be static
+    
+
+    public static void main(String[] args) {
+        System.err.println("Hello");
+    }
+}
---
+++ Array/prg/Asearch.java
@@ -0,0 +1,23 @@
+
+package prg;
+
+/**
+ * Asearch
+ */
+public  class Asearch {
+
+
+    static int norSearchM(int arr[],int element){
+
+        int loop=0;
+        int arraySize=arr.length-1;
+        while (loop<=arraySize) {
+            if (element==arr[loop]) {
+                
+            }
+        }
+        return -1;
+    }
+
+    
+}
